d70edfcdd9e926290e63ec277985a611983c583b
msknmehdi <msknmehdi@gmail.com>
2014-12-10 21:23:26 +0100

    Trombinoscope / absences

--- application/controllers/TrombinoscopeController.php
+++ application/controllers/TrombinoscopeController.php
@@ -48,7 +48,8 @@ class TrombinoscopeController extends Zend_Controller_Action
     }
     
     public function mehdiamaskaneAction(){
-    
+    	$dbUti = new Model_DbTable_Flux_Uti();
+    	$this->view->data = $dbUti->findByRole($this->_getParam('role', "THYP 14-15"),true, true);
     }
     
 	public function zinebslaouiAction(){
--- public/js/amaskanemehdi.js
+++ public/js/amaskanemehdi.js
@@ -1,16 +1,11 @@
 $(function(){
-	$(".img").each(function(){
-		$(this).click(function(){
-			alert('ok');
-		});
-		//$(this).mouseover(function(){
-			//$(this).parent().css("z-index","100");
-			//$(this).removeClass('img');
-		//});
-		//$(this).mouseout(function(){
-			//$(this).addClass('img');
-			//$(this).parent().removeAttr("style");
-		//});
-		
-	});
+	
 });
+
+function setAbsence(usr){
+	alert("en développement");
+}
+
+function setPresence(usr){
+	alert("en développement");
+}
